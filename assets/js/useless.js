5c1869cfd6ae8b882070b970a1b13ffcfbff2102
Joshua King <joshua-coding@Joshuas-MacBook-Pro.local>
2023-07-31 21:36:41 -0700

    Updated JS changed Git Random gif btn

--- assets/js/useless.js
+++ assets/js/useless.js
@@ -2,14 +2,15 @@
 document.getElementById("getFactButton").addEventListener("click", function() {
   getRandomUselessFact();
 });
+
 function getRandomUselessFact() {
-  const requestURL = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+  var requestURL = "https://uselessfacts.jsph.pl/api/v2/facts/random";
 
   fetch(requestURL)
     .then(response => response.json())
     .then(data => {
       if (data && data.text) {
-        const fact = data.text;
+        var fact = data.text;
         displayFact(fact);
       } else {
         console.error('Error fetching useless fact: Invalid response data');
@@ -19,6 +20,7 @@ function getRandomUselessFact() {
       console.error('Error fetching useless fact:', error);
     });
 }
+
 function displayFact(fact) {
   document.getElementById("factDisplay").textContent = 'Random Useless Fact:\n' + fact;
 }
